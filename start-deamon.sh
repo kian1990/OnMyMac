6fba878083b62a8c4ccbd633e132191c447e69d2
kian1990 <269590936@qq.com>
2025-02-28 17:42:19 +0800

    Add files via upload

---
+++ start-deamon.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# 查找进程PID
+pid=$(pgrep DeamonName)
+
+# 判断PID是否为空
+if [ -z "$pid" ]; then
+    cd /opt/app
+    nohup /opt/app/DeamonName > /opt/app/DeamonName.log 2>&1 &
+    echo "已启动"
+else
+    echo "运行中"
+fi
---
+++ stop-deamon.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# 查找进程PID
+pid=$(pgrep DeamonName)
+
+# 判断PID是否为空
+if [ -z "$pid" ]; then
+	echo "未运行"
+else
+    kill -9 $pid > /dev/null 2>&1
+    echo "已结束"
+fi
