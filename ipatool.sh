dd19c850298d479f8fb207a3e4a4ba90a3f9e78d
kian1990 <269590936@qq.com>
2025-08-30 21:12:20 +0800

    Create ipatool.sh

---
+++ ipatool.sh
@@ -0,0 +1,7 @@
+https://github.com/majd/ipatool
+
+ipatool auth login
+ipatool search spofity
+ipatool list-versions -b com.spotify.client
+ipatool get-version-metadata -b com.spotify.client --external-version-id 874468913
+ipatool download -b com.spotify.client --external-version-id 874468913
